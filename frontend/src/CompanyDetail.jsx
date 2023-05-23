e047a4cb8b206934e9a1a45a48ed5f541c28a3af
nguarino522 <sub4miler5@gmail.com>
2023-05-22 21:21:15 -0400

    issue with company detail page

--- frontend/src/CompanyDetail.jsx
+++ frontend/src/CompanyDetail.jsx
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"
 import JoblyApi from "./JoblyApi";
+import JobCardList from "./JobCardList";
+import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import LoadingSpinner from "./LoadingSpinner";
 
 const CompanyDetail = () => {
     const { handle } = useParams();
+    const [company, setCompany] = useState([]);
 
-    const [company, setCompany] = useState(null);
+    useEffect(() => {
+        async function getCompany() {
+            let result = await JoblyApi.getCompany(handle);
+            setCompany(result);
+        }
+        getCompany();
+    }, [handle]);
+
+    if (!company) return <LoadingSpinner />;
 
     return (
-        <div>
-            temp HOLD: {handle}
-        </div>
+        <Container>
+            <div className="CompanyDetail col-md-8 offset-md-2">
+                <h4>{company.name}</h4>
+                <p>{company.description}</p>
+                <Row>
+                    <JobCardList jobs={company.jobs} />
+                </Row>
+            </div>
+        </Container>
     )
 
 }
